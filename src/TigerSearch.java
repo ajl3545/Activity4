d11877fbf898e8fb64998c230406ef0459558c0e
Alex Lamarche <ajl3545@rit.edu>
2019-11-14 22:02:09 -0500

    Added new files

--- src/TigerSearch.java
+++ src/TigerSearch.java
@@ -7,24 +7,27 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
 import java.util.*;
-public class UseCase1Test {
+public class TigerSearch {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
+    System.setProperty("webdriver.chrome.driver","WebDrivers/chromedriver.exe");
+    driver = new ChromeDriver(new ChromeOptions());
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
@@ -37,6 +40,13 @@ public class UseCase1Test {
     driver.get("https://tigercenter.rit.edu/tigerCenterApp/landing");
     driver.manage().window().setSize(new Dimension(1920, 1080));
     driver.findElement(By.cssSelector(".img-text:nth-child(2)")).click();
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
     {
       WebElement element = driver.findElement(By.name("termSelector"));
       Actions builder = new Actions(driver);
@@ -58,6 +68,13 @@ public class UseCase1Test {
     driver.findElement(By.cssSelector(".completer-input")).sendKeys(Keys.ENTER);
     driver.findElement(By.cssSelector(".classSearchAdvancedSearchText")).click();
     {
+
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
       WebElement dropdown = driver.findElement(By.xpath("//div/div/div[3]/div/select"));
       dropdown.findElement(By.xpath("//option[. = 'RIT Main']")).click();
     }
